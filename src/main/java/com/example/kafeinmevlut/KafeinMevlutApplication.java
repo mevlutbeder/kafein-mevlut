e9e0b0075bcb6f756b0c8ed469eda983c0a29987
Mevlut <Mev.!!''^^99>
2022-12-25 15:59:39 +0300

    Initial commit

---
+++ src/main/java/com/example/kafeinmevlut/KafeinMevlutApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafeinmevlut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafeinMevlutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafeinMevlutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kafeinmevlut/KafeinMevlutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafeinmevlut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafeinMevlutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
